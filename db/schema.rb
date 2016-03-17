62041a205cab42efc2e9c693c801a2275f610147
thanglyphan <lyern52@gmail.com>
2016-03-17 16:40:50 +0100

    lol

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315140643) do
+ActiveRecord::Schema.define(version: 20160317153901) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",           limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160315140643) do
     t.string   "salt",               limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "gender",             limit: 255
   end
 
 end
