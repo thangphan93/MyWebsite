bc6c3ff164ba8b54cea540e30bd896653fbab2cd
thanglyphan <lyern52@gmail.com>
2016-03-18 20:00:06 +0100

    add admin boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318151037) do
+ActiveRecord::Schema.define(version: 20160318185928) do
 
   create_table "items", force: :cascade do |t|
     t.string   "program",    limit: 255
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160318151037) do
     t.datetime "updated_at"
     t.string   "gender",             limit: 255
     t.string   "program",            limit: 255
+    t.boolean  "admin"
   end
 
 end
