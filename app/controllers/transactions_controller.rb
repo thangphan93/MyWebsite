08405b918e97a6a569ffd92c0e8799e02d565f55
thanglyphan <lyern52@gmail.com>
2016-03-21 12:41:06 +0100

    d

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,23 +1,25 @@
 class TransactionsController < ApplicationController
   def new
-    gon.client_token = generate_client_token
+    gon.client_token = generate_new_client_token
   end
 
   def create
     @result = Braintree::Transaction.sale(
-        amount: 5, #MONEY HERE FROM ITEM
-        payment_method_nonce: params[:payment_method_nonce])
+              amount: 5, #MONEY HERE FROM ITEM
+              payment_method_nonce: params[:payment_method_nonce])
     if @result.success?
       redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
-      gon.client_token = generate_client_token
+      gon.client_token = generate_new_client_token
       render :new
     end
   end
 
+
+
   private
-  def generate_client_token
+  def generate_new_client_token
     Braintree::ClientToken.generate
   end
 end
