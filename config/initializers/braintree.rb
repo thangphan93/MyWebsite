fe6a5ba01216f342978c23cdeb2637ba90574250
thanglyphan <lyern52@gmail.com>
2016-03-21 13:11:18 +0100

    d

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,5 +1,11 @@
-Braintree::Configuration.environment = :production #:sandbox#
+Braintree::Configuration.environment = :sandbox #:sandbox#production
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
-Braintree::Configuration.merchant_id = 'fqmh5bkjbcbz5t57'#ENV['BRAINTREE_MERCHANT_ID']
-Braintree::Configuration.public_key = 'z2ddq3cqkf4kbtzt'#ENV['BRAINTREE_PUBLIC_KEY']
-Braintree::Configuration.private_key = '0b451d6104a672ee425fc44fe244588f'#ENV['BRAINTREE_PRIVATE_KEY']
+#FAKE SHIT
+Braintree::Configuration.merchant_id = 'n25tvnzzsmbx675x'#ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = 'mw4c263nbmz83rfm'#ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = '0345f1f1e06a7d5f3c4304d2e0d1e922'#ENV['BRAINTREE_PRIVATE_KEY']
+
+#REAL SHIT
+#Braintree::Configuration.merchant_id = 'fqmh5bkjbcbz5t57'#ENV['BRAINTREE_MERCHANT_ID']
+#Braintree::Configuration.public_key = 'z2ddq3cqkf4kbtzt'#ENV['BRAINTREE_PUBLIC_KEY']
+#Braintree::Configuration.private_key = '0b451d6104a672ee425fc44fe244588f'#ENV['BRAINTREE_PRIVATE_KEY']
