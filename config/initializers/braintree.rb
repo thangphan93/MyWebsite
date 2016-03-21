59b0d0c4708ea601679523f78e3a703903a8ae2e
thanglyphan <lyern52@gmail.com>
2016-03-21 12:58:16 +0100

    d

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,5 +1,5 @@
 Braintree::Configuration.environment = :production #:sandbox#
 Braintree::Configuration.logger = Logger.new('log/braintree.log')
-Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
-Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
-Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
+Braintree::Configuration.merchant_id = 'fqmh5bkjbcbz5t57'#ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = 'z2ddq3cqkf4kbtzt'#ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = '0b451d6104a672ee425fc44fe244588f'#ENV['BRAINTREE_PRIVATE_KEY']
