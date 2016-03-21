e8663df23262c6b1fff9bbea694a03c4314f9936
thanglyphan <lyern52@gmail.com>
2016-03-21 14:49:49 +0100

    d

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   post 'update_items',        :to => 'admins#update_items'
   post 'delete_items',        :to => 'admins#delete_items'
   post 'add_items',           :to => 'admins#add_items'
-  post 'transactions',        :to => 'transactions#create'
+  post 'transactions',        :to => 'transactions#new'
 
   resources :transactions, only: [:new, :create]
   resources :users, only: [:new, :create]
