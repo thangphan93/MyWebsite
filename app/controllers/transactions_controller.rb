75be587798b318049a2db678f46cddd56981ca1c
thanglyphan <lyern52@gmail.com>
2016-03-21 13:17:53 +0100

    d

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,7 +8,8 @@ class TransactionsController < ApplicationController
               amount: 5, #MONEY HERE FROM ITEM
               payment_method_nonce: params[:payment_method_nonce])
     if @result.success?
-      redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
+      flash[:notice] = "Congraulations! Your transaction has been successfully!"
+      redirect_to(:action => 'login', :controller => 'sessions')
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
       gon.client_token = generate_new_client_token
